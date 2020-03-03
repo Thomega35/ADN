d8846424ff210cbeb009dea4ae012d1e6aaec01d
CodyAdam <cody.adam@laposte.net>
2020-03-03 16:07:50 +0100

    Initial commit

---
+++ src/fr/istic/si2/adnmatch/app_v1.scala
@@ -0,0 +1,15 @@
+package fr.istic.si2.adnmatch
+
+import fr.istic.si2.scribble._
+import fr.istic.si2.adnmatch._
+import fr.istic.si2.adnmatch.FonctionsRExp._
+import fr.istic.si2.adnmatchlib._
+
+/**
+ * Application ADNMatch version 1.
+ */
+object ADNMatchV1 extends App {
+
+  println("ADNMatch Version 1")
+  // TODO V1 - A compléter
+}
---
+++ src/fr/istic/si2/adnmatch/rexp_basique.scala
@@ -0,0 +1,54 @@
+package fr.istic.si2.adnmatch
+
+import fr.istic.si2.scribble._
+import fr.istic.si2.adnmatch._
+
+/**
+ * Type algébrique modélisant
+ * les bases azotées composant les séquences ADN.
+ */
+sealed trait Base
+case object A extends Base
+case object T extends Base
+case object G extends Base
+case object C extends Base
+
+/**
+ * Type algébrique récursif modélisant les
+ * expressions régulières sur les bases azotées.
+ */
+sealed trait RExp
+case object Impossible extends RExp
+case object Vide extends RExp
+case object Nqb extends RExp
+case class UneBase(b: Base) extends RExp
+case class Choix(e1: RExp, e2: RExp) extends RExp
+case class Concat(e1: RExp, e2: RExp) extends RExp
+case class Repete(e: RExp) extends RExp
+case class NFois(e: RExp, n: Int) extends RExp
+
+object FonctionsRExp {
+
+  /**
+   * @param lb une liste de bases azotées
+   * @return une chaîne de caractères représentant les bases de lb, dans l'ordre
+   */
+  // TODO V1
+  def listeBasesToString(lb: List[Base]): String = ???
+
+  /**
+   * @param e une expression régulière
+   * @return la représentation textuelle de e, avec toutes les parenthèses nécessaires
+   */
+  // TODO V1
+  def rExpToString(e: RExp): String = ???
+
+  /**
+   * @param e une expression régulière
+   * @return une liste de bases obtenue en déroulant e tout le temps de la même manière.
+   * @note Indiquez ici vos choix réalisés pour les répétitions, les choix, les Nqb.
+   */
+  // TODO V1
+  def deroule(e: RExp): Option[List[Base]] = ???
+
+}
